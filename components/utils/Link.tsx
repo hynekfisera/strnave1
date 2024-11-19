1ed667ac70e59d104691ff5a9fdf9c1cc5ec495a
Hynek Fišera <hynek.fisera@seznam.cz>
2024-11-19 16:38:20 +0100

    Update Link.tsx

--- components/utils/Link.tsx
+++ components/utils/Link.tsx
@@ -11,7 +11,7 @@ export default function Link({ href, className, children }: Props) {
 
   hrefBuilder += process.env.NEXT_PUBLIC_BASE_URL ?? "";
 
-  if (!href.includes("index")) {
+  if (!href.includes("index") || process.env.NODE_ENV !== "development") {
     hrefBuilder += href;
   }
 
