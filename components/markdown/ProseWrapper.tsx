1e35e4a9166b1428105850c3b8e9abea6ae8d9da
Hynek Fišera <hynek.fisera@seznam.cz>
2025-01-09 18:39:50 +0100

    Add content

--- components/markdown/ProseWrapper.tsx
+++ components/markdown/ProseWrapper.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function ProseWrapper({ children, className }: Props) {
   return (
     <div
-      className={`py-12 px-6 md:px-0 prose max-w-none prose-p:text-justify prose-a:text-sky-600 prose-headings:text-gray-800 prose-h1:text-2xl md:prose-h1:text-3xl ${className ?? ""}`}
+      className={`py-12 px-6 md:px-0 prose max-w-none prose-p:text-justify prose-a:text-sky-600 hover:prose-a:text-sky-700 prose-a:transition prose-a:duration-100 prose-headings:text-gray-800 prose-h1:text-2xl md:prose-h1:text-3xl ${className ?? ""}`}
     >
       {children}
     </div>
